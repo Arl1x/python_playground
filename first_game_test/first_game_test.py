b44a34a07ccca75db69f9b9ed35973758ad63049
David Kraemer <david.anthony.kraemer@gmail.com>
2018-07-31 10:55:31 -0400

    updated first_game_test.py to pep8 standards and created display_test folder

--- first_game_test/first_game_test.py
+++ first_game_test/first_game_test.py
@@ -11,25 +11,29 @@ pygame.init()
 display_width = 800
 display_height = 600
 
-# Not sure why we need the variable, but the rest of this sets the 
+# Not sure why we need the variable, but the rest of this sets the
 # display to a pixel height/width as defined in the variables. We also
 # set the title bar of the game window.
-game_display = pygame.display.set_mode((display_width,display_height))
+game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('First Game Test')
 
 # Here we define the event handler function that, for this test, drives
 # the rest of the logic within the "game"
+
+
 def event_handler():
-	for event in pygame.event.get():
-		#print(event)
-		if event.type == QUIT or (
-			event.type == KEYDOWN and (
-				event.key == K_ESCAPE or
-				event.key == K_q
-			)):
-			pygame.quit()
-			quit()
+    for event in pygame.event.get():
+        # print(event)
+        if event.type == QUIT or (
+            event.type == KEYDOWN and (
+                event.key == K_ESCAPE or
+                event.key == K_q
+            )
+        ):
+            pygame.quit()
+            quit()
+
 
 while True:
-	event_handler()
-	pygame.display.update()
+    event_handler()
+    pygame.display.update()
